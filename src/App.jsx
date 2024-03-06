0dfa504f8e05a7fcd2e38cee9052f5701f2f4a53
Jovany S. Opelina <102904687+bankaihekai@users.noreply.github.com>
2024-03-06 17:10:05 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,57 @@
 import { useState, useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Import Bootstrap JavaScript library
 import MainMapDisplay from './MainMapDisplay.jsx';
+import './App.css'; // Import custom CSS for additional styling
+import { FaBars } from "react-icons/fa6";
 
 function App() {
-    const [location, setLocation] = useState({ latitude: null, longitude: null });
-    const [error, setError] = useState(null);
-    console.log(error);
-    // fetch current location 
-    useEffect(() => {
-        const options = {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 0
-        };
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  const [error, setError] = useState(null);
 
-        const successCallback = (position) => {
-            setLocation({
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude
-            });
-        };
+  // Fetch current location 
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
 
-        const errorCallback = (error) => {
-            setError(error.message);
-        };
+    const successCallback = (position) => {
+      setLocation({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      });
+    };
 
-        const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, options);
+    const errorCallback = (error) => {
+      setError(error.message);
+    };
 
-        return () => {
-            navigator.geolocation.clearWatch(watchId);
-        };
-    }, []);
+    const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback, options);
 
-    return (
-        <div>
-            <MainMapDisplay latitude={location.latitude ?? 0} longitude={location.longitude ?? 0} />
+    return () => {
+      navigator.geolocation.clearWatch(watchId);
+    };
+  }, []);
+
+  return (
+    <div>
+      <button className="btn btn-primary floating-button" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><FaBars /></button>
+
+      <div className="offcanvas offcanvas-start" data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-    );
+        <div className="offcanvas-body">
+          <p>Try scrolling the rest of the page to see this option in action.</p>
+        </div>
+      </div>
+
+      <MainMapDisplay latitude={location.latitude ?? 0} longitude={location.longitude ?? 0} />
+    </div>
+  );
 }
 
 export default App;
